130bb6a48a3b7df76db7c0e58c4e7ea9d4f96536
byusluerreulsuyb <lu.usluer@gmail.com>
2019-05-15 16:57:47 +0300

    vet.idex and controller page implemented

---
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/controllers/VetController.java
@@ -0,0 +1,20 @@
+package com.petclinic.byusluer.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+
+    @RequestMapping({"/vets","/vets/index","/vets/index.html"})
+    public String listVets(){
+
+
+
+
+
+        return "vets/vet_index";
+    }
+}
