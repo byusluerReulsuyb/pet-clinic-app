c905b8a475ea8c0abdda14b16ffd5fe9a75baca7
byusluerreulsuyb <lu.usluer@gmail.com>
2019-05-17 01:05:43 +0300

    adding owner list

--- pet-clinic-web/src/main/java/com/petclinic/byusluer/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/controllers/VetController.java
@@ -1,15 +1,25 @@
 package com.petclinic.byusluer.controllers;
 
 
+import com.petclinic.byusluer.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final OwnerService ownerService;
+
+
+    public VetController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+
 
 
 
