16118cb711207f05750882bd7f20780f28408a97
byusluerreulsuyb <lu.usluer@gmail.com>
2019-05-23 16:30:37 +0300

    added visit class, updated owner with pet relationship

--- pet-clinic-data/src/main/java/com/petclinic/byusluer/model/Owner.java
+++ pet-clinic-data/src/main/java/com/petclinic/byusluer/model/Owner.java
@@ -1,11 +1,24 @@
 package com.petclinic.byusluer.model;
 
+import java.util.Set;
+
 public class Owner extends com.petclinic.byusluer.model.Person {
+
+    private Set<Pet> pets;
 
     public Owner() {
     }
 
     public Owner(String firstName, String lastName) {
         super(firstName, lastName);
+    }
+
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
     }
 }
--- pet-clinic-web/src/main/java/com/petclinic/byusluer/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/controllers/VetController.java
@@ -19,7 +19,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"","/","index",".html"})
+    @RequestMapping({""})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
