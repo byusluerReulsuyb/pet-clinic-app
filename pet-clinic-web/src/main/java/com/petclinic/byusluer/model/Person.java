2e6379848022b98bf72de58c8b778d3021956722
byusluerreulsuyb <lu.usluer@gmail.com>
2019-06-07 23:31:14 +0300

    updated owner class with JPA

--- pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Person.java
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Person.java
@@ -1,12 +1,18 @@
 package com.petclinic.byusluer.model;
 
-public class Person extends BaseEntity{
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class Person extends BaseEntity {
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
 
     public Person() {
     }
-
-    private String firstName;
-    private String lastName;
 
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
