469a301f91f8d8af5cc0140b7a1db50a6b97ebee
byusluerreulsuyb <lu.usluer@gmail.com>
2019-06-07 23:44:09 +0300

    update vet&speciality to jpa entity

--- pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Specialty.java
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Specialty.java
@@ -1,7 +1,15 @@
 package com.petclinic.byusluer.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Specialty extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
--- pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Vet.java
+++ pet-clinic-web/src/main/java/com/petclinic/byusluer/model/Vet.java
@@ -1,14 +1,20 @@
 package com.petclinic.byusluer.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Entity
+@Table(name = "vets")
 public class Vet extends  Person{
 
     public Vet(String firstName, String lastName) {
         super(firstName, lastName);
     }
-
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "vet_id"),
+               inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialities = new HashSet<>();
 
     public Set<Specialty> getSpecialities() {
